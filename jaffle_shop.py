bf78a2ccd5ff9458f3f51972d1d8dc45b4605acf
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2023-12-27 16:36:05 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -5,12 +5,19 @@ from airflow.models import Variable
 from cosmos import DbtTaskGroup, RenderConfig
 from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
 from cosmos.profiles import PostgresUserPasswordProfileMapping
+
 from pathlib import Path
+
+CONNECTION_ID = "postgres_connection"
+SCHEMA_NAME = "public"
+DB_NAME = "postgres"
+POSTGRES_USER = "postgres"
+POSTGRES_PASSWORD = Variable.get("AIRFLOW_POSTGRES_PASSWORD")
 
 profile_config = ProfileConfig(
     profile_name="jaffle_shop",
     target_name="dev",
-    profiles_yml_filepath = "/appz/home/airflow/dags/dbt/jaffle_shop/profiles.yml",
+    profiles_yml_filepath = "/appz/home/airflow/dags/dbt/jaffle_shop/profiles.yml"
 )
 
 with DAG(
@@ -18,64 +25,23 @@ with DAG(
     start_date=datetime(2023, 11, 10),
     schedule_interval="0 0 * 1 *",
 ):
-    e1 = EmptyOperator(task_id="pre_dbt")
-
-    seeds_tg = DbtTaskGroup(
-        project_config=ProjectConfig(
-        Path("/appz/home/airflow/dags/dbt/jaffle_shop"),
-    ),
-        operator_args={
-            "append_env": True,
-        },
-        profile_config=profile_config,
-        execution_config=ExecutionConfig(
-        dbt_executable_path="/dbt_venv/bin/dbt",
-    ),
-        render_config=RenderConfig(
-        select=["path:seeds/"],
-    ),
-        default_args={"retries": 2},
-        group_id = "dbt_seeds_group"
-    )
-
-    stg_tg = DbtTaskGroup(
-        project_config=ProjectConfig(
-        Path("/appz/home/airflow/dags/dbt/jaffle_shop"),
-    ),
-        operator_args={
-            "append_env": True,
-        },
-        profile_config=profile_config,
-        execution_config=ExecutionConfig(
-        dbt_executable_path="/dbt_venv/bin/dbt",
-    ),
-        render_config=RenderConfig(
-        select=["path:models/staging/"],
-    ),
-        default_args={"retries": 2},
-        group_id = "dbt_stg_group"
-    )
+     e1 = EmptyOperator(task_id="pre_dbt")
 
     dbt_tg = DbtTaskGroup(
         project_config=ProjectConfig(
         Path("/appz/home/airflow/dags/dbt/jaffle_shop"),
     ),
-        operator_args={
-            "append_env": True,
-        },
         profile_config=profile_config,
         execution_config=ExecutionConfig(
         dbt_executable_path="/dbt_venv/bin/dbt",
     ),
         render_config=RenderConfig(
-        exclude=["path:models/staging","path:seeds/"],
+        exclude=["path:models/staging"],
     ),
         default_args={"retries": 2},
     )
-   
+
     e2 = EmptyOperator(task_id="post_dbt")
-    
 
-    e1 >> seeds_tg >> stg_tg >> dbt_tg >> e2
-   
-
+    e1 >> dbt_tg >> e2
+    #end
