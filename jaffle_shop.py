6c9cdb0ad1c32098e4712bd62bcc486e1bf98946
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-01-02 15:12:17 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -1,10 +1,8 @@
 from pendulum import datetime
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
-from airflow.models import Variable
-from cosmos import DbtTaskGroup, RenderConfig
+from cosmos import DbtTaskGroup
 from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
-from cosmos.profiles import PostgresUserPasswordProfileMapping
 
 from pathlib import Path
 
@@ -24,7 +22,7 @@ with DAG(
 
     # DBT Task Group 1
     dbt_tg1 = DbtTaskGroup(
-        group_id="dbt_task_group_1",  # Unique group_id
+        group_id="dbt_task_group_1",
         project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop")),
         operator_args={"append_env": True},
         profile_config=profile_config,
@@ -34,7 +32,7 @@ with DAG(
 
     # DBT Task Group 2
     dbt_tg2 = DbtTaskGroup(
-        group_id="dbt_task_group_2",  # Unique group_id
+        group_id="dbt_task_group_2",
         project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop")),
         operator_args={"append_env": True},
         profile_config=profile_config,
@@ -48,5 +46,4 @@ with DAG(
     # Define task dependencies
     pre_dbt >> dbt_tg1 >> post_dbt
     pre_dbt >> dbt_tg2 >> post_dbt
-
 #
