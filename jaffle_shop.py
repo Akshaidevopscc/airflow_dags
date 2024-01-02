c0c68b92c2927e3b069f06d28edb8b6b54343a45
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-01-02 14:46:48 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -11,30 +11,61 @@ from pathlib import Path
 profile_config = ProfileConfig(
     profile_name="jaffle_shop",
     target_name="dev",
-    profiles_yml_filepath = "/appz/home/airflow/dags/dbt/jaffle_shop/profiles.yml"
+    profiles_yml_filepath="/appz/home/airflow/dags/dbt/jaffle_shop/profiles.yml"
 )
 
 with DAG(
-    dag_id="jaffle_shop_new",
-    start_date=datetime(2023, 11, 10),
-    schedule_interval="0 0 * 1 *",
+        dag_id="jaffle_shop_new",
+        start_date=datetime(2023, 11, 10),
+        schedule_interval="0 0 * 1 *",
 ):
     e1 = EmptyOperator(task_id="pre_dbt")
 
-    dbt_tg = DbtTaskGroup(
+    dbt_tg_1 = DbtTaskGroup(
+        task_id="dbt_tg_1",
         project_config=ProjectConfig(
-        Path("/appz/home/airflow/dags/dbt/jaffle_shop"),
-    ),
+            Path("/appz/home/airflow/dags/dbt/jaffle_shop"),
+        ),
         operator_args={
             "append_env": True,
         },
         profile_config=profile_config,
         execution_config=ExecutionConfig(
-        dbt_executable_path="/dbt_venv/bin/dbt",
-    ),
+            dbt_executable_path="/dbt_venv/bin/dbt",
+        ),
+        default_args={"retries": 2},
+    )
+
+    dbt_tg_2 = DbtTaskGroup(
+        task_id="dbt_tg_2",
+        project_config=ProjectConfig(
+            Path("/appz/home/airflow/dags/dbt/jaffle_shop"),
+        ),
+        operator_args={
+            "append_env": True,
+        },
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(
+            dbt_executable_path="/dbt_venv/bin/dbt",
+        ),
+        default_args={"retries": 2},
+    )
+
+    dbt_tg_3 = DbtTaskGroup(
+        task_id="dbt_tg_3",
+        project_config=ProjectConfig(
+            Path("/appz/home/airflow/dags/dbt/jaffle_shop"),
+        ),
+        operator_args={
+            "append_env": True,
+        },
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(
+            dbt_executable_path="/dbt_venv/bin/dbt",
+        ),
         default_args={"retries": 2},
     )
 
     e2 = EmptyOperator(task_id="post_dbt")
 
-    e1 >> dbt_tg >> e2
+    e1 >> dbt_tg_1 >> dbt_tg_2 >> dbt_tg_3 >> e2
