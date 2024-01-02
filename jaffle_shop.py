a917df25951cc5e0371dc1ebac8fd761395fe146
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-01-02 18:11:01 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -1,7 +1,6 @@
 from pendulum import datetime
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator
 from cosmos import DbtTaskGroup, RenderConfig
 from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
 from pathlib import Path
@@ -12,20 +11,12 @@ profile_config = ProfileConfig(
     profiles_yml_filepath="/appz/home/airflow/dags/dbt/jaffle_shop/profiles.yml",
 )
 
-def print_variable(**kwargs):
-    variable = kwargs['dag_run'].conf.get('payment_type')
-    print(variable)
-
 with DAG(
     dag_id="jaffle_shop_new",
     start_date=datetime(2023, 11, 10),
     schedule_interval="0 0 * 1 *",
 ) as dag:
-    e1 = PythonOperator(
-        task_id="print_variables",
-        python_callable=print_variable,
-        provide_context=True,
-    )
+    e1 = EmptyOperator(task_id="pre_dbt")
 
     seeds_tg = DbtTaskGroup(
         task_id="seeds_tg",
