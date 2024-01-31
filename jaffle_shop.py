07588db22e9e41a8fb7563d4f54eb421ff7c4be8
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-01-31 14:26:04 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -53,6 +53,7 @@ def check_and_clear_task():
         if dag_run_status == "running":
             print("DAG run completed successfully.")
             break
+            
         elif dag_run_status == "failed":
             print("DAG run failed. Initiating task clearing...")
             task_clear(profile="PRO", Dag="airflow_dags_akshai", dag_run_id="scheduled__2024-01-29T00:00:00+00:00", task_ids=["pre_dbt", "dbt_seeds_group", "dbt_final_group", "post_dbt"])
