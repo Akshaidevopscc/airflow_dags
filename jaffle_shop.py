9da0d8b2fba2d22e64e0ef9f6bf6daa569c45e47
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-01-31 12:01:14 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -4,12 +4,46 @@ from airflow.operators.empty import EmptyOperator
 from cosmos import DbtTaskGroup, RenderConfig
 from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
 from pathlib import Path
+import requests
+import time
+import json
+sys.path.append("/appz/home/airflow/dags/airflow_dags_akshai")
+from clear_task import task_clear
 
 profile_config = ProfileConfig(
     profile_name="jaffle_shop",
     target_name="dev",
     profiles_yml_filepath="/appz/home/airflow/dags/dbt/jaffle_shop_akshai/profiles.yml",
 )
+
+def check_dag_status(dag_id, dag_run_id, profile):
+    cred_path = f"{profile}.json"
+    try:
+        with open(cred_path) as file:
+            credentials = json.load(file)
+    except Exception as e:
+        print(f"Error: {e}")
+        print("Credentials not found.")
+        return
+
+    username = credentials["Username"]
+    password = credentials["Password"]
+    domain = credentials["Domain"]
+
+    uri = f"https://{domain}/api/v1/dags/{dag_id}/dagRuns/{dag_run_id}"
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    response = requests.get(uri, auth=(username, password), headers=headers)
+
+    if response.status_code == 200:
+        dag_run_details = response.json()
+        return dag_run_details.get("state")
+    else:
+        print(f"Failed to retrieve DAG Run details. Status Code: {response.status_code}")
+        return None
 
 with DAG(
     dag_id="airflow_dags_akshai",
@@ -52,3 +86,16 @@ with DAG(
     e2 = EmptyOperator(task_id="post_dbt")
 
     e1 >> seeds_tg >> stg_tg >> dbt_tg >> e2
+
+while True:
+    dag_run_status = check_dag_status("airflow_dags_akshai", "scheduled__2024-01-30T00:00:00+00:00", "PRO")
+    if dag_run_status == "success":
+        print("DAG run completed successfully.")
+        break
+    elif dag_run_status == "failed":
+        print("DAG run failed. Initiating task clearing...")
+        task_clear(profile="PRO", Dag="airflow_dags_akshai", dag_run_id="scheduled__2024-01-29T00:00:00+00:00", task_ids=["pre_dbt", "dbt_seeds_group", "dbt_final_group", "post_dbt"])
+        break
+    else:
+        print("DAG run is still in progress. Waiting for 10 seconds before checking again...")
+        time.sleep(10)
