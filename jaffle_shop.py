1efeb297b07fa38f73755b2dedd735637f72ff69
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-01 17:08:25 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -55,7 +55,7 @@ def check_and_clear_task():
 
     while True:
         dag_run_status = check_dag_status("airflow_dags_akshai", "scheduled__2024-01-30T00:00:00+00:00", "PRO")
-        if dag_run_status == "success":
+        if dag_run_status == "running":
             print("DAG run completed successfully.")
             break
         elif dag_run_status == "failed":
