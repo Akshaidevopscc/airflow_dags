62aacc880c107873256c11787953fe78f522de2e
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-02 10:52:45 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -1,40 +1,19 @@
+from pendulum import datetime
 from airflow import DAG
-from airflow.models import DagRun, TaskInstance
-from airflow.utils.session import create_session
-from airflow.utils.state import State
-from datetime import datetime
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.exceptions import AirflowException
 from cosmos import DbtTaskGroup, RenderConfig
 from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
 from pathlib import Path
+class IntentionalFailureException(AirflowException):
+    pass
 
 profile_config = ProfileConfig(
     profile_name="jaffle_shop",
     target_name="dev",
     profiles_yml_filepath="/appz/home/airflow/dags/dbt/jaffle_shop_akshai/profiles.yml",
 )
-
-def clear_failed_tasks(target_dag_id, target_dag_run_id):
-
-    with create_session() as session:
-        dag_run = session.query(DagRun).filter(
-            DagRun.dag_id == target_dag_id,
-            DagRun.run_id == target_dag_run_id
-        ).first()
-        
-        if not dag_run:
-            raise ValueError(f'DAGRun not found for DAG ID {target_dag_id} and Run ID {target_dag_run_id}')
-
-        failed_task_instances = session.query(TaskInstance).filter(
-            TaskInstance.dag_id == target_dag_id,
-            TaskInstance.run_id == target_dag_run_id,
-            TaskInstance.state == State.FAILED
-        ).all()
-        
-        for ti in failed_task_instances:
-            ti.clear()
-            session.add(ti)
 
 with DAG(
     dag_id="airflow_dags_akshai",
@@ -73,13 +52,13 @@ with DAG(
         render_config=RenderConfig(exclude=["path:models/staging", "path:seeds/"]),
         default_args={"retries": 2},
     )
+    raise IntentionalFailureException("Intentional failure occurred in dbt_tg stage")
 
     e2 = EmptyOperator(task_id="post_dbt")
 
-    clear_failed_tasks_op = PythonOperator(
-        task_id="clear_failed_tasks",
-        python_callable=clear_failed_tasks,
-        op_kwargs={"target_dag_id": "airflow_dags_akshai", "target_dag_run_id": "scheduled__2024-01-30T00:00:00+00:00"},
+    check_and_clear_task_op = PythonOperator(
+        task_id="check_and_clear_task",
+        python_callable=check_and_clear_task,
     )
 
-    e1 >> seeds_tg >> stg_tg >> dbt_tg >> e2 >> clear_failed_tasks_op
+    e1 >> seeds_tg >> stg_tg >> dbt_tg >> e2 >> check_and_clear_task_op
