505d8da2dff91f3fac8594fc2297adf7b3939c3d
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-04 19:19:09 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -1,13 +1,9 @@
+from pendulum import datetime
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.python_operator import PythonOperator
-from airflow.models import TaskInstance
-from airflow.utils.session import create_session
 from cosmos import DbtTaskGroup, RenderConfig
 from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
 from pathlib import Path
-from airflow.operators.bash_operator import BashOperator
-from datetime import datetime, timedelta
 
 profile_config = ProfileConfig(
     profile_name="jaffle_shop",
@@ -15,42 +11,17 @@ profile_config = ProfileConfig(
     profiles_yml_filepath="/appz/home/airflow/dags/dbt/jaffle_shop_akshai/profiles.yml",
 )
 
-def clear_upstream_task(context):
-    execution_date = context.get("execution_date")
-    clear_tasks = BashOperator(
-        task_id='clear_tasks',
-        bash_command=f'airflow tasks clear -s {execution_date} -t pre_dbt -d -y airflow_dags_akshai'
-    )
-    return clear_tasks.execute(context=context)
-
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(seconds=5),
-    'on_failure_callback': clear_upstream_task 
-}
-
 with DAG(
     dag_id="airflow_dags_akshai",
     start_date=datetime(2023, 11, 10),
     schedule_interval="0 0 * 1 *",
-    default_args=default_args,
-    catchup=False
 ) as dag:
 
     e1 = EmptyOperator(task_id="pre_dbt")
 
-    seeds_tg = DbtTaskGroup(
-        group_id="dbt_seeds_group",
-        project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
-        operator_args={"append_env": True},
-        profile_config=profile_config,
-        execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
-        render_config=RenderConfig(select=["path:seeds/"]),
-        default_args={"retries": 2},
+    seeds_tg = BashOperator(
+        task_id="seeds_tg",
+        bash_command="exit 1",
     )
 
     stg_tg = DbtTaskGroup(
