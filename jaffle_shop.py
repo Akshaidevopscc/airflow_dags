2e69f80cb0c674fc9ccc0cd2f6e207c507a64d82
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-04 22:06:34 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -56,19 +56,9 @@ with DAG(
 
     e1 = EmptyOperator(task_id="pre_dbt")
 
-    '''seeds_tg = DbtTaskGroup(
-        group_id="dbt_seeds_group",
-        project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
-        operator_args={"append_env": True},
-        profile_config=profile_config,  
-        execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
-        render_config=RenderConfig(select=["path:seeds/"]),
-        default_args={"retries": 2},
-    )'''
-
     seeds_tg = BashOperator(
     task_id="seeds_tg",
-    bash_command="exit 1",  # Command to simulate failure
+    bash_command="exit 1", 
     )
 
     stg_tg = DbtTaskGroup(
