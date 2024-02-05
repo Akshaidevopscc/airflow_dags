4a87964963932bee141c5b799a0554ca387fad1a
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-05 10:40:41 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -1,10 +1,13 @@
-from pendulum import datetime
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import TaskInstance
+from airflow.utils.session import create_session
 from cosmos import DbtTaskGroup, RenderConfig
 from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
-from airflow.operators.bash_operator import BashOperator
 from pathlib import Path
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
 
 profile_config = ProfileConfig(
     profile_name="jaffle_shop",
@@ -12,48 +15,38 @@ profile_config = ProfileConfig(
     profiles_yml_filepath="/appz/home/airflow/dags/dbt/jaffle_shop_akshai/profiles.yml",
 )
 
-def clear_upstream_task(task_instance, context, **kwargs):
+def clear_upstream_tasks(context):
     execution_date = context.get("execution_date")
     clear_tasks = BashOperator(
         task_id='clear_tasks',
-        bash_command=f'airflow tasks clear -s {execution_date} -t seeds_tg -d -y clear_upstream_task',
+        bash_command=f'airflow tasks clear -s {execution_date} -t downstream_task_id -d -y <your_dag_id>'
     )
-    return clear_tasks
+    return clear_tasks.execute(context=context)
 
-with DAG(
-    dag_id="airflow_dags_akshai",
-    start_date=datetime(2023, 11, 10),
-    schedule_interval="0 0 * 1 *",
-) as dag:
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
+}
 
-    e1 = EmptyOperator(task_id="pre_dbt")
+with DAG('your_dag_id',
+         start_date=datetime(2021, 1, 1),
+         max_active_runs=3,
+         schedule_interval=timedelta(minutes=5),
+         default_args=default_args,
+         catchup=False
+         ) as dag:
 
-    seeds_tg = BashOperator(
-        task_id="seeds_tg",
-        bash_command="exit 1",  
-        on_failure_callback=clear_upstream_task 
-    )
-
-    stg_tg = DbtTaskGroup(
-        group_id="dbt_stg_group",
-        project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
-        operator_args={"append_env": True},
-        profile_config=profile_config,
-        execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
-        render_config=RenderConfig(select=["path:models/staging/"]),
-        default_args={"retries": 2},
-    )
-
-    dbt_tg = DbtTaskGroup(
-        group_id="dbt_final_group",
-        project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
-        operator_args={"append_env": True},
-        profile_config=profile_config,
-        execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
-        render_config=RenderConfig(exclude=["path:models/staging", "path:seeds/"]),
-        default_args={"retries": 2},
+    t0 = DummyOperator(task_id='t0')
+    t1 = DummyOperator(task_id='t1')
+    t2 = DummyOperator(task_id='t2')
+    t3 = BashOperator(
+        task_id='t3',
+        bash_command='exit 123',
+        on_failure_callback=clear_upstream_tasks
     )
 
-    e2 = EmptyOperator(task_id="post_dbt")
-
-    e1 >> seeds_tg >> stg_tg >> dbt_tg >> e2
+    t0 >> t1 >> t2 >> t3
