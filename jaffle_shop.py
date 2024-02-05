8e469bb0c810cbf85697d2fbb8341ca2be3dda36
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-05 17:33:59 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -2,25 +2,16 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
-from airflow.utils.dag_processing import SimpleDag
+
 
 def clear_upstream_task(context):
     execution_date = context.get("execution_date")
-    dag = context['dag']
-    dag_bag = context['dag_bag']
-    task_instance = context['task_instance']
-    upstream_task_ids = dag.get_task(task_instance.task_id).upstream_task_ids
-    for task_id in upstream_task_ids:
-        dag.clear(
-            start_date=execution_date,
-            end_date=execution_date,
-            only_failed=True,
-            include_subdags=True,
-            reset_dag_runs=True,
-            task_regex=task_id,
-            include_parentdag_runs=True,
-        )
-        print(f"Cleared upstream tasks for task {task_id}")
+    clear_tasks = BashOperator(
+        task_id='clear_tasks',
+        bash_command=f'airflow tasks clear -s {execution_date}  -t t2 -d -y clear_upstream_task'
+    )
+    return clear_tasks.execute(context=context)
+
 
 # Default settings applied to all tasks
 default_args = {
@@ -32,6 +23,7 @@ default_args = {
     'retry_delay': timedelta(seconds=5)
 }
 
+
 with DAG('clear_upstream_task',
          start_date=datetime(2021, 1, 1),
          max_active_runs=3,
@@ -42,18 +34,20 @@ with DAG('clear_upstream_task',
     t0 = DummyOperator(
         task_id='t0'
     )
+
     t1 = DummyOperator(
         task_id='t1'
     )
+
     t2 = DummyOperator(
         task_id='t2'
     )
     t3 = BashOperator(
         task_id='t3',
         bash_command='exit 123',
+        #retries=1,
         on_failure_callback=clear_upstream_task
     )
-    t0 >> t1 >> t2 >> t3
 
-###############################################
-#
+    t0 >> t1 >> t2 >> t3
+################################################
