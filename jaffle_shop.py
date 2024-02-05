133e1db841a7752c3d107bb980fed906d36628f1
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-05 10:51:59 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -1,43 +1,46 @@
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
-from airflow.operators.python_operator import PythonOperator
-from airflow.models import TaskInstance
-from airflow.utils.session import create_session
-from cosmos import DbtTaskGroup, RenderConfig
-from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
-from pathlib import Path
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
-from pendulum import datetime
 
-profile_config = ProfileConfig(
-    profile_name="jaffle_shop",
-    target_name="dev",
-    profiles_yml_filepath="/appz/home/airflow/dags/dbt/jaffle_shop_akshai/profiles.yml",
-)
-
-def clear_upstream_tasks(context):
+def clear_upstream_task(context):
     execution_date = context.get("execution_date")
     clear_tasks = BashOperator(
         task_id='clear_tasks',
-        bash_command=f'airflow tasks clear -s {execution_date} -t downstream_task_id -d -y airflow_dags_akshai'
+        bash_command=f'airflow tasks clear -s {execution_date} -t t2 -u -y clear_upstream_task'
     )
-    return clear_tasks.execute(context=context)
+    clear_tasks.execute(context=context)
 
-with DAG(
-    dag_id="airflow_dags_akshai",
-    start_date=datetime(2023, 11, 10),
-    schedule_interval="0 0 * 1 *",
-) as dag:
+# Default settings applied to all tasks
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
+}
 
-    t0 = EmptyOperator(task_id='t0')
-    t1 = EmptyOperator(task_id='t1')
-    t2 = EmptyOperator(task_id='t2')
-    t3 = PythonOperator(
+with DAG('clear_upstream_task',
+         start_date=datetime(2021, 1, 1),
+         max_active_runs=3,
+         schedule_interval=timedelta(minutes=5),
+         default_args=default_args,
+         catchup=False
+         ) as dag:
+    t0 = DummyOperator(
+        task_id='t0'
+    )
+    t1 = DummyOperator(
+        task_id='t1'
+    )
+    t2 = DummyOperator(
+        task_id='t2'
+    )
+    t3 = BashOperator(
         task_id='t3',
-        python_callable=your_python_function,
-        on_failure_callback=clear_upstream_tasks
+        bash_command='exit 123',
+        on_failure_callback=clear_upstream_task
     )
 
     t0 >> t1 >> t2 >> t3
-####
