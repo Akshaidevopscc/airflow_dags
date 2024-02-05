d20246727560ae126cc802519aab07e01ea411b6
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-05 16:48:08 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -1,92 +1,46 @@
-from pendulum import datetime
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
-from airflow.operators.python_operator import PythonOperator
-from cosmos import DbtTaskGroup, RenderConfig
-from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
-from pathlib import Path
-import requests
-import time
-import json
-import sys
-sys.path.append("/appz/home/airflow/dags/airflow_dags_akshai")
-from clear_task import task_clear
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.api.client.local_client import Client
+from datetime import datetime, timedelta
 
-USERNAME = "apitest"
-PASSWORD = "mnbvcxz"
-DOMAIN = "mpmathew-test-poc.03907124.lowtouch.cloud"
+def clear_upstream_task(context):
+    execution_date = context.get("execution_date")
+    client = Client(None)
+    upstream_tasks = context['task'].get_direct_relatives(upstream=True)
+    for task in upstream_tasks:
+        task_instance = client.get_task_instance(task.task_id, execution_date)
+        task_instance.clear()
 
-def check_and_clear_task():
-    def check_dag_status(dag_id, dag_run_id, username, password, domain):
-        uri = f"https://{domain}/api/v1/dags/{dag_id}/dagRuns/{dag_run_id}"
-        headers = {"Content-Type": "application/json"}
-        response = requests.get(uri, auth=(username, password), headers=headers)
-        if response.status_code == 200:
-            dag_run_details = response.json()
-            return dag_run_details.get("state")
-        else:
-            print(f"Failed to retrieve DAG Run details. Status Code: {response.status_code}")
-            return None
+# Default settings applied to all tasks
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
+}
 
-    while True:
-        dag_run_status = check_dag_status("airflow_dags_akshai", "scheduled__2024-01-30T00:00:00+00:00", USERNAME, PASSWORD, DOMAIN)
-        if dag_run_status in ["running", "success"]:
-            print("DAG run completed successfully.")
-            break
-        elif dag_run_status in ["failed"]:
-            print("DAG run failed. Initiating task clearing...")
-            task_clear(profile="PRO", username=USERNAME, password=PASSWORD, domain=DOMAIN, dag_id="airflow_dags_akshai", dag_run_id="scheduled__2024-01-30T00:00:00+00:00", task_ids=["pre_dbt", "dbt_seeds_group", "dbt_final_group", "post_dbt"])
-            break
-
-profile_config = ProfileConfig(
-    profile_name="jaffle_shop",
-    target_name="dev",
-    profiles_yml_filepath="/appz/home/airflow/dags/dbt/jaffle_shop_akshai/profiles.yml",
-)
-
-with DAG(
-    dag_id="airflow_dags_akshai",
-    start_date=datetime(2023, 11, 10),
-    schedule_interval="0 0 * 1 *",
-) as dag:
-
-    e1 = EmptyOperator(task_id="pre_dbt")
-
-    seeds_tg = DbtTaskGroup(
-        group_id="dbt_seeds_group",
-        project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
-        operator_args={"append_env": True},
-        profile_config=profile_config,
-        execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
-        render_config=RenderConfig(select=["path:seeds/"]),
-        default_args={"retries": 2},
+with DAG('clear_upstream_task',
+         start_date=datetime(2021, 1, 1),
+         max_active_runs=3,
+         schedule_interval=timedelta(minutes=5),
+         default_args=default_args,
+         catchup=False
+         ) as dag:
+    t0 = DummyOperator(
+        task_id='t0'
     )
-
-    stg_tg = DbtTaskGroup(
-        group_id="dbt_stg_group",
-        project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
-        operator_args={"append_env": True},
-        profile_config=profile_config,
-        execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
-        render_config=RenderConfig(select=["path:models/staging/"]),
-        default_args={"retries": 2},
+    t1 = DummyOperator(
+        task_id='t1'
     )
-
-    dbt_tg = DbtTaskGroup(
-        group_id="dbt_final_group",
-        project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
-        operator_args={"append_env": True},
-        profile_config=profile_config,
-        execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
-        render_config=RenderConfig(exclude=["path:models/staging", "path:seeds/"]),
-        default_args={"retries": 2},
+    t2 = DummyOperator(
+        task_id='t2'
     )
-
-    e2 = EmptyOperator(task_id="post_dbt")
-
-    check_and_clear_task_op = PythonOperator(
-        task_id="check_and_clear_task",
-        python_callable=check_and_clear_task,
+    t3 = BashOperator(
+        task_id='t3',
+        bash_command='exit 123',
+        on_failure_callback=clear_upstream_task
     )
-
-    e1 >> seeds_tg >> stg_tg >> dbt_tg >> e2 >> check_and_clear_task_op
+    t0 >> t1 >> t2 >> t3
