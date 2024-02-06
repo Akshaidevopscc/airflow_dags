1c668c6f93c5b3987ea2b458ddb0cbd16902819e
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-06 19:23:34 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -1,26 +1,26 @@
-from airflow import DAG
+from airflow import DAG, settings
+from airflow.models import DagRun
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
-def clear_upstream_task(context):
-    execution_date = context.get("execution_date")
-    dag = context['dag']
-    task_instance = context['task_instance']
-    upstream_task_ids = dag.get_task(task_instance.task_id).upstream_task_ids
-    print(upstream_task_ids)
-    for task_id in upstream_task_ids:
-        dag.clear(
-            start_date=execution_date,
-            end_date=execution_date,
-            dry_run=False,
-            only_failed=False,
-            only_running=False,
-            include_subdags=True,
-            include_parentdag=True,
-            task_ids=[task_id],
+def clear_failed_tasks_of_another_dag(context, target_dag_id, target_dag_run_id):
+    session = settings.Session()
+    try:
+        # Find the target DAG run
+        target_dag_run = (
+            session.query(DagRun)
+            .filter(DagRun.dag_id == target_dag_id, DagRun.run_id == target_dag_run_id)
+            .one()
         )
-        print("Cleared upstream tasks for task {}".format(task_id))
+
+        # Clear failed tasks of the target DAG run
+        target_dag_run.clear()
+        print(f"Cleared failed tasks of DAG run {target_dag_run_id} in DAG {target_dag_id}")
+    except Exception as e:
+        print(f"Error clearing failed tasks of DAG run {target_dag_run_id} in DAG {target_dag_id}: {e}")
+    finally:
+        session.close()
 
 default_args = {
     'owner': 'airflow',
@@ -34,25 +34,25 @@ default_args = {
 with DAG('clear_upstream_task',
          start_date=datetime(2021, 1, 1),
          max_active_runs=3,
-         schedule_interval=timedelta(minutes=5),
+         schedule_interval=timedelta(minutes=30),
          default_args=default_args,
          catchup=False
          ) as dag:
     t0 = DummyOperator(
         task_id='t0',
-        on_failure_callback=clear_upstream_task
+        on_failure_callback=lambda context: clear_failed_tasks_of_another_dag(context, "manual", "manual__2024-02-06T13:40:35.029208+00:00")
     )
     t1 = DummyOperator(
         task_id='t1',
-        on_failure_callback=clear_upstream_task
+        on_failure_callback=lambda context: clear_failed_tasks_of_another_dag(context, "manual", "manual__2024-02-06T13:40:35.029208+00:00")
     )
     t2 = DummyOperator(
         task_id='t2',
-        on_failure_callback=clear_upstream_task
+        on_failure_callback=lambda context: clear_failed_tasks_of_another_dag(context, "manual", "manual__2024-02-06T13:40:35.029208+00:00")
     )
     t3 = BashOperator(
         task_id='t3',
         bash_command='exit 123',
-        on_failure_callback=clear_upstream_task
+        on_failure_callback=lambda context: clear_failed_tasks_of_another_dag(context, "manual", "manual__2024-02-06T13:40:35.029208+00:00")
     )
     t0 >> t1 >> t2 >> t3
