670f5d09103cc5421d0028d179a1e45f5951a7c9
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-06 16:53:46 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -1,14 +1,21 @@
+from pendulum import datetime
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.bash_operator import BashOperator
-from datetime import datetime, timedelta
+from airflow.operators.empty import EmptyOperator
+from cosmos import DbtTaskGroup, RenderConfig
+from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
+from pathlib import Path
+
+profile_config = ProfileConfig(
+    profile_name="jaffle_shop",
+    target_name="dev",
+    profiles_yml_filepath="/appz/home/airflow/dags/dbt/jaffle_shop_akshai/profiles.yml",
+)
 
 def clear_upstream_task(context):
     execution_date = context.get("execution_date")
     dag = context['dag']
     task_instance = context['task_instance']
     upstream_task_ids = dag.get_task(task_instance.task_id).upstream_task_ids
-    print('**********************************************************************************')
     print(upstream_task_ids)
     for task_id in upstream_task_ids:
         dag.clear(
@@ -21,41 +28,49 @@ def clear_upstream_task(context):
             include_parentdag=True,
             task_ids=[task_id],
         )
-        print('**********************************************************************************')
-        print(task_id)
         print("Cleared upstream tasks for task {}".format(task_id))
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(seconds=5)
-}
+with DAG(
+    dag_id="airflow_dags_akshai",
+    start_date=datetime(2023, 11, 10),
+    schedule_interval="0 0 * 1 *",
+) as dag:
 
-with DAG('clear_upstream_task',
-         start_date=datetime(2021, 1, 1),
-         max_active_runs=3,
-         schedule_interval=timedelta(minutes=5),
-         default_args=default_args,
-         catchup=False
-         ) as dag:
-    t0 = DummyOperator(
-        task_id='t0',
+    e1 = EmptyOperator(task_id="pre_dbt", on_failure_callback=clear_upstream_task)
+
+    seeds_tg = DbtTaskGroup(
+        group_id="dbt_seeds_group",
+        project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
+        operator_args={"append_env": True},
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
+        render_config=RenderConfig(select=["path:seeds/"]),
+        default_args={"retries": 2},
         on_failure_callback=clear_upstream_task
     )
-    t1 = BashOperator(
-        task_id='t1',
-        bash_command='exit 123',
+
+    stg_tg = DbtTaskGroup(
+        group_id="dbt_stg_group",
+        project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
+        operator_args={"append_env": True},
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
+        render_config=RenderConfig(select=["path:models/staging/"]),
+        default_args={"retries": 2},
         on_failure_callback=clear_upstream_task
     )
-    t2 = DummyOperator(
-        task_id='t2',
+
+    dbt_tg = DbtTaskGroup(
+        group_id="dbt_final_group",
+        project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
+        operator_args={"append_env": True},
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
+        render_config=RenderConfig(exclude=["path:models/staging", "path:seeds/"]),
+        default_args={"retries": 2},
         on_failure_callback=clear_upstream_task
     )
-    t3 = DummyOperator(
-        task_id='t3',
-        on_failure_callback=clear_upstream_task
-    )
-    t0 >> t1 >> t2 >> t3
+
+    e2 = EmptyOperator(task_id="post_dbt", on_failure_callback=clear_upstream_task)
+
+    e1 >> seeds_tg >> stg_tg >> dbt_tg >> e2
