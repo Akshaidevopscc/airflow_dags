72bfc9ff456c6fc32bac336f451ea1fbdc9f56df
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-07 16:14:43 +0530

    Create clear_failed_task.py

---
+++ clear_failed_task.py
@@ -0,0 +1,10 @@
+from airflow import DAG
+from airflow.models import DagRun
+
+def failed_tasks(target_dag_id, target_dag_run_id):
+    dagruns = DagRun.find(dag_id=target_dag_id, run_id=target_dag_run_id)
+    if dagruns:
+        for dagrun in dagruns:
+            for ti in dagrun.get_task_instances():
+                if ti.state == 'failed':
+                    ti.set_state('none')
