228d2d5aec90572e760161a949099e0766945e04
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-07 19:50:28 +0530

    Create airflow_shop.py

---
+++ airflow_shop.py
@@ -0,0 +1,55 @@
+from pendulum import datetime
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from cosmos import DbtTaskGroup, RenderConfig
+from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
+from pathlib import Path
+
+profile_config = ProfileConfig(
+    profile_name="airflow_shop", 
+    target_name="dev",
+    profiles_yml_filepath="/appz/home/airflow/dags/dbt/jaffle_shop_akshai/profiles.yml", 
+)
+
+with DAG(
+    dag_id="airflow_shop",
+    start_date=datetime(2023, 11, 10), 
+    schedule_interval="0 0 * 1 *",
+) as dag:
+
+    e1 = EmptyOperator(task_id="pre_dbt")
+
+    seeds_tg = DbtTaskGroup(
+        group_id="dbt_seeds_group",
+        project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
+        operator_args={"append_env": True},
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
+        render_config=RenderConfig(select=["path:seeds/"]),
+        default_args={"retries": 2},
+    )
+
+    stg_tg = DbtTaskGroup(
+        group_id="dbt_stg_group",
+        project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
+        operator_args={"append_env": True},
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
+        render_config=RenderConfig(select=["path:models/staging/"]),
+        default_args={"retries": 2},
+    )
+
+    dbt_tg = DbtTaskGroup(
+        group_id="dbt_final_group",
+        project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
+        operator_args={"append_env": True},
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
+        render_config=RenderConfig(exclude=["path:models/staging", "path:seeds/"]),
+        default_args={"retries": 2},
+    )
+
+    e2 = EmptyOperator(task_id="post_dbt")
+
+    e1 >> seeds_tg >> stg_tg >> dbt_tg >> e2
+
