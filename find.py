ec4a446d729607e36b483e1c6446b1562c062a4f
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-09 13:08:10 +0530

    Create find.py

---
+++ find.py
@@ -0,0 +1,88 @@
+from datetime import datetime
+from typing import List, Optional, Union
+
+from airflow import DAG
+from airflow.models.dagrun import DagRun
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils import timezone
+from airflow.utils.db import provide_session
+from sqlalchemy.orm.session import Session
+
+
+class MyDagRun(DagRun):
+
+    @staticmethod
+    @provide_session
+    def find(
+        dag_id: Optional[Union[str, List[str]]] = None,
+        run_id: Optional[str] = None,
+        execution_date: Optional[datetime] = None,
+        state: Optional[str] = None,
+        external_trigger: Optional[bool] = None,
+        no_backfills: bool = False,
+        session: Session = None,
+        execution_start_date: Optional[datetime] = None,
+        execution_end_date: Optional[datetime] = None,
+    ) -> List["DagRun"]:
+
+        DR = MyDagRun
+
+        qry = session.query(DR)
+        dag_ids = [dag_id] if isinstance(dag_id, str) else dag_id
+        if dag_ids:
+            qry = qry.filter(DR.dag_id.in_(dag_ids))
+        if run_id:
+            qry = qry.filter(DR.run_id == run_id)
+        if execution_date:
+            if isinstance(execution_date, list):
+                qry = qry.filter(DR.execution_date.in_(execution_date))
+            else:
+                qry = qry.filter(DR.execution_date == execution_date)
+        if execution_start_date and execution_end_date:
+            qry = qry.filter(DR.execution_date.between(execution_start_date, execution_end_date))
+        elif execution_start_date:
+            qry = qry.filter(DR.execution_date >= execution_start_date)
+        elif execution_end_date:
+            qry = qry.filter(DR.execution_date <= execution_end_date)
+        if state:
+            qry = qry.filter(DR.state == state)
+        if external_trigger is not None:
+            qry = qry.filter(DR.external_trigger == external_trigger)
+        if no_backfills:
+            # in order to prevent a circular dependency
+            from airflow.jobs import BackfillJob
+            qry = qry.filter(DR.run_id.notlike(BackfillJob.ID_PREFIX + '%'))
+
+        return qry.order_by(DR.execution_date).all()
+
+
+def func(**kwargs):
+    dag_id = ''  # Update this with your desired DAG ID
+    dr = MyDagRun()
+    # Need to use timezone to avoid ValueError: naive datetime is disallowed
+    start = timezone.make_aware(datetime(2021, 3, 1, 9, 59, 0))  # change to your required start
+    end = timezone.make_aware(datetime(2021, 3, 1, 10, 1, 0))  # change to your required end
+    results = dr.find(dag_id=dag_id,
+                      execution_start_date=start,
+                      execution_end_date=end
+                      )
+
+    print("Execution dates met criteria are:")
+    for item in results:
+        print(item.execution_date)
+    return results
+
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2019, 11, 1),
+}
+
+with DAG(dag_id='test',
+         default_args=default_args,
+         schedule_interval=None,
+         catchup=True
+         ) as dag:
+
+    op = PythonOperator(task_id="task",
+                        python_callable=func)
