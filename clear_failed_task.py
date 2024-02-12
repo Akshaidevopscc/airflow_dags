568bb51add6832b2006147f8f1ed1ef66d71094f
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-12 16:38:46 +0530

    Update clear_failed_task.py

--- clear_failed_task.py
+++ clear_failed_task.py
@@ -1,3 +1,4 @@
+#######
 from datetime import datetime
 from typing import List, Optional, Union
 from airflow import DAG
@@ -6,6 +7,8 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.utils import timezone
 from airflow.utils.db import provide_session
 from sqlalchemy.orm.session import Session
+
+# Importing the necessary modules from the previous snippet
 from airflow.models import DagRun
 from airflow.models.taskinstance import TaskInstance
 
@@ -49,10 +52,13 @@ class MyDagRun(DagRun):
         if external_trigger is not None:
             qry = qry.filter(DR.external_trigger == external_trigger)
         if no_backfills:
+            # in order to prevent a circular dependency
             from airflow.jobs import BackfillJob
             qry = qry.filter(DR.run_id.notlike(BackfillJob.ID_PREFIX + '%'))
+
         return qry.order_by(DR.execution_date).all()
-        
+
+# Function to clear failed tasks
 def clear_failed_tasks(target_dag_id, target_dag_run_id):
     dagruns = DagRun.find(dag_id=target_dag_id, run_id=target_dag_run_id)
     if dagruns:
@@ -61,13 +67,18 @@ def clear_failed_tasks(target_dag_id, target_dag_run_id):
                 if ti.state == 'failed':
                     ti.set_state('none')
 
+# Function to retrieve dag run IDs and clear failed tasks
 def func(**kwargs):
     dag_id = 'airflow_dags_akshai'
     dr = MyDagRun()
     results = dr.find(dag_id=dag_id)
+
     dag_run_ids = [dag_run.run_id for dag_run in results]
+
+    # Loop through dag run IDs and clear failed tasks
     for run_id in dag_run_ids:
         clear_failed_tasks(dag_id, run_id)
+
     return dag_run_ids
 
 default_args = {
@@ -80,6 +91,6 @@ with DAG(dag_id='test',
          schedule_interval=None,
          catchup=True
          ) as dag:
-             
+
     op = PythonOperator(task_id="task",
                         python_callable=func)
