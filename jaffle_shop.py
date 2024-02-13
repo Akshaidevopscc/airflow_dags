a56a9f0256049dbb732d9fb311c1e643f78740b2
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-13 16:23:51 +0530

    Update jaffle_shop.py

--- jaffle_shop.py
+++ jaffle_shop.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="jaflle_shop",
+    dag_id="jaffle_shop",
     start_date=datetime(2023, 11, 10),
     schedule_interval="0 0 * 1 *",
 ) as dag:
