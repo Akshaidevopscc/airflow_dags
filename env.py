e3219beaf0206b51762ef3bf04d4b5993612a6b0
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-21 14:29:33 +0530

    Update env.py

--- env.py
+++ env.py
@@ -1,23 +1,23 @@
 from pendulum import datetime
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
-from cosmos import DbtTaskGroup, RenderConfig
+from cosmos import DbtTaskGroup, RenderConfig, LoadMode
 from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
+from cosmos.constants import TestBehavior
+from airflow.models import Variable
 from pathlib import Path
+
+AIRFLOW_USER = "airflow"
+POSTGRES_TEST_PASSWORD = Variable.get("AIRFLOW_POSTGRES_TEST_PASSWORD")
 
 profile_config = ProfileConfig(
     profile_name="jaffle_shop",
     target_name="dev",
     profiles_yml_filepath="/appz/home/airflow/dags/dbt/jaffle_shop_akshai/profiles.yml",
-    dbt_vars = {
-    "my_dbt_var": "my_value",
-    "start_time": "{{ data_interval_start.strftime('%Y%m%d%H%M%S') }}",
-    "end_time": "{{ data_interval_end.strftime('%Y%m%d%H%M%S') }}",
-},
 )
 
 with DAG(
-    dag_id="env",
+    dag_id="env_test",
     start_date=datetime(2023, 11, 10),
     schedule=None,
     catchup=False,
@@ -28,30 +28,30 @@ with DAG(
     seeds_tg = DbtTaskGroup(
         group_id="dbt_seeds_group",
         project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
-        operator_args={"append_env": True},
+        env_vars={"AIRFLOW_POSTGRES_TEST_USER": AIRFLOW_USER,"AIRFLOW_POSTGRES_TEST_PASSWORD": POSTGRES_TEST_PASSWORD},
         profile_config=profile_config,
         execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
-        render_config=RenderConfig(select=["path:seeds/"]),
+        render_config=RenderConfig(load_method=LoadMode.DBT_LS,select=["path:seeds/"]),
         default_args={"retries": 2},
     )
 
     stg_tg = DbtTaskGroup(
         group_id="dbt_stg_group",
         project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
-        operator_args={"append_env": True},
+        env_vars={"AIRFLOW_POSTGRES_TEST_USER": AIRFLOW_USER,"AIRFLOW_POSTGRES_TEST_PASSWORD": POSTGRES_TEST_PASSWORD},
         profile_config=profile_config,
         execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
-        render_config=RenderConfig(select=["path:models/staging/"]),
+        render_config=RenderConfig(load_method=LoadMode.DBT_LS,select=["path:models/staging/"]),
         default_args={"retries": 2},
     )
 
     dbt_tg = DbtTaskGroup(
         group_id="dbt_final_group",
         project_config=ProjectConfig(Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")),
-        operator_args={"append_env": True},
+        env_vars={"AIRFLOW_POSTGRES_TEST_USER": AIRFLOW_USER,"AIRFLOW_POSTGRES_TEST_PASSWORD": POSTGRES_TEST_PASSWORD},
         profile_config=profile_config,
         execution_config=ExecutionConfig(dbt_executable_path="/dbt_venv/bin/dbt"),
-        render_config=RenderConfig(exclude=["path:models/staging", "path:seeds/"]),
+        render_config=RenderConfig(load_method=LoadMode.DBT_LS,exclude=["path:models/staging", "path:seeds/"]),
         default_args={"retries": 2},
     )
 
