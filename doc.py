476f64db874c9d0b4564e0d95b5a0077274f7c67
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-02-26 15:11:12 +0530

    Update doc.py

--- doc.py
+++ doc.py
@@ -9,19 +9,22 @@ POSTGRES_TEST_PASSWORD = Variable.get("AIRFLOW_POSTGRES_TEST_PASSWORD")
 PATH_TO_DBT_PROJECT = "/appz/home/airflow/dags/dbt/jaffle_shop"
 PATH_TO_DBT_VENV = "/dbt_venv/bin/dbt"
 
-
 @dag(
     start_date=datetime(2023, 3, 23),
-    schedule=None,
+    schedule_interval=None,  
     catchup=False,
+    tags=['dbt']  
 )
 def simple_dbt_dag():
     dbt_run = BashOperator(
         task_id="dbt_run",
-        bash_command="$PATH_TO_DBT_VENV docs generate",
-        env={"PATH_TO_DBT_VENV": PATH_TO_DBT_VENV,"AIRFLOW_POSTGRES_TEST_USER": AIRFLOW_USER,"AIRFLOW_POSTGRES_TEST_PASSWORD": POSTGRES_TEST_PASSWORD},
+        bash_command=f"{PATH_TO_DBT_VENV} run",
+        env={
+            "PATH_TO_DBT_VENV": PATH_TO_DBT_VENV,
+            "AIRFLOW_POSTGRES_TEST_USER": AIRFLOW_USER,
+            "AIRFLOW_POSTGRES_TEST_PASSWORD": POSTGRES_TEST_PASSWORD
+        },
         cwd=PATH_TO_DBT_PROJECT,
     )
-
 
 simple_dbt_dag()
