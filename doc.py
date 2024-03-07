334a71834f7e41b8a4f1c439e52818780eeb01c0
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-03-07 14:19:54 +0530

    Update doc.py

--- doc.py
+++ doc.py
@@ -16,18 +16,18 @@ with DAG(
 
     project_path = Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")
     dbt_executable_path = "/dbt_venv/bin/dbt"
-    target_directory = "/appz/home/airflow/docs/"  # Specify the target directory here
+    target_directory = "/appz/home/airflow/docs/" 
 
     dbt_generate_docs = BashOperator(
     task_id="dbt_generate_docs",
-    bash_command=f"{dbt_executable_path} docs generate --target dev --output /appz/home/airflow/docs/",
+    bash_command=f"{dbt_executable_path} docs generate --target dev /appz/home/airflow/docs/",
     env={
         "AIRFLOW_POSTGRES_TEST_USER": AIRFLOW_USER,
         "AIRFLOW_POSTGRES_TEST_PASSWORD": POSTGRES_TEST_PASSWORD
     },
     cwd=project_path,
 )
-
+    
     dbt_serve_docs = BashOperator(
         task_id="dbt_serve_docs",
         bash_command=f"{dbt_executable_path} docs serve &",
