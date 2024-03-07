01ac6d6db02a3f64cfe780c100b048b59416973a
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-03-07 13:16:50 +0530

    Update doc.py

--- doc.py
+++ doc.py
@@ -7,6 +7,9 @@ from pathlib import Path
 AIRFLOW_USER = "airflow"
 POSTGRES_TEST_PASSWORD = Variable.get("AIRFLOW_POSTGRES_TEST_PASSWORD")
 
+def get_docs_path(dag_id):
+    return f"/appz/home/airflow/docs/{dag_id}"
+
 with DAG(
     dag_id="doc_generate",
     start_date=datetime(2023, 11, 10),
@@ -16,6 +19,9 @@ with DAG(
 
     project_path = Path("/appz/home/airflow/dags/dbt/jaffle_shop_akshai")
     dbt_executable_path = "/dbt_venv/bin/dbt"
+    
+    dag_id = dag.dag_id
+    docs_path = get_docs_path(dag_id)
     
     dbt_generate_and_serve_docs = BashOperator(
         task_id="dbt_generate_and_serve_docs",
