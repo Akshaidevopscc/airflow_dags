1fac7255dbb1efabd6160750a98e0bbf72c05384
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-03-13 15:46:26 +0530

    Update doc.py

--- doc.py
+++ doc.py
@@ -4,6 +4,9 @@ from airflow.operators.bash import BashOperator
 from airflow.models import Variable
 from pathlib import Path
 
+AIRFLOW_USER = "airflow"
+POSTGRES_TEST_PASSWORD = Variable.get("AIRFLOW_POSTGRES_TEST_PASSWORD")
+
 with DAG(
     dag_id="doc_generate",
     start_date=datetime(2023, 11, 10),
@@ -17,6 +20,10 @@ with DAG(
     dbt_generate_docs = BashOperator(
         task_id="dbt_generate_docs",
         bash_command=f"{dbt_executable_path} docs generate --target dev",
+        env={
+            "AIRFLOW_POSTGRES_TEST_USER": AIRFLOW_USER,
+            "AIRFLOW_POSTGRES_TEST_PASSWORD": POSTGRES_TEST_PASSWORD
+        },
         cwd=project_path,
     )
 
