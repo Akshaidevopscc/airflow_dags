a821e77ccfccb99fcc929a2cb34566c78cafbc93
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-03-18 19:09:43 +0530

    Update doc.py

--- doc.py
+++ doc.py
@@ -1,50 +1,36 @@
+from pendulum import datetime
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.plugins_manager import AirflowPlugin
-from flask import Blueprint, send_from_directory
-import os
-from datetime import datetime
+from airflow.operators.bash import BashOperator
+from pathlib import Path
 
-# Blueprint for the plugin
-docs_blueprint = Blueprint(
-    'docs_blueprint', __name__,
-    url_prefix='/docs',
-    template_folder='templates',
-    static_folder='/appz/home/airflow/docs'
-)
-
-@docs_blueprint.route('/jaffle_shop')
-def custom_static(filename):
-    return send_from_directory(docs_blueprint.static_folder, filename)
-
-class StaticDocsPlugin(AirflowPlugin):
-    name = "static_docs_plugin"
-    flask_blueprints = [docs_blueprint]
-
-def run_my_dag():
-    # Define your tasks here
-    pass
-
-# Define the default arguments for the DAG
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2024, 3, 18),
-    'retries': 1,
-}
-
-# Instantiate the DAG
-with DAG('my_static_docs_dag', 
-         default_args=default_args,
-         schedule_interval=None,  # Set the schedule interval as needed
-         catchup=False) as dag:
-
-    # Define tasks
-    run_this = PythonOperator(
-        task_id='run_my_dag_task',
-        python_callable=run_my_dag
+with DAG(
+    dag_id="doc_generate",
+    start_date=datetime(2023, 11, 10),
+    schedule=None,
+    catchup=False,
+) as dag:
+    
+    dbt_executable_path = "/dbt_venv/bin/dbt"
+    
+    dbt_generate_docs_1 = BashOperator(
+        task_id="dbt_generate_docs_1",
+        bash_command = f"{dbt_executable_path} docs generate --target dev --project-dir /appz/home/airflow/dags/dbt/jaffle_shop_akshai --profiles-dir /appz/home/airflow/dags/dbt/jaffle_shop_akshai"
     )
 
-# Define task dependencies
-run_this
+    dbt_serve_docs_1 = BashOperator(
+        task_id="dbt_serve_docs_1",
+        bash_command = f"{dbt_executable_path} docs serve --target dev --project-dir /appz/home/airflow/dags/dbt/jaffle_shop_akshai --profiles-dir /appz/home/airflow/dags/dbt/jaffle_shop_akshai &"
+    )
 
+    dbt_generate_docs_2 = BashOperator(
+        task_id="dbt_generate_docs_2",
+        bash_command = f"{dbt_executable_path} docs generate --target dev --project-dir /appz/home/airflow/dags/dbt/data_engineering --profiles-dir /appz/home/airflow/dags/dbt/data_engineering"
+    )
+
+    dbt_serve_docs_2 = BashOperator(
+        task_id="dbt_serve_docs_2",
+        bash_command = f"{dbt_executable_path} docs serve --target dev --port 9090 --project-dir /appz/home/airflow/dags/dbt/data_engineering --profiles-dir /appz/home/airflow/dags/dbt/data_engineering &"
+    )
+
+    dbt_generate_docs_1 >> dbt_serve_docs_1 >> dbt_generate_docs_2 >> dbt_serve_docs_2
 
