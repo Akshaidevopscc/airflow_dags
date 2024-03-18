96219a68276c214d4e79b7a3243f374d974cf161
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-03-18 18:25:04 +0530

    Update doc.py

--- doc.py
+++ doc.py
@@ -13,22 +13,22 @@ with DAG(
     dbt_executable_path = "/dbt_venv/bin/dbt"
     
     dbt_generate_docs_1 = BashOperator(
-        task_id="dbt_generate_docs",
+        task_id="dbt_generate_docs_1",
         bash_command = f"{dbt_executable_path} docs generate --target dev --project-dir /appz/home/airflow/dags/dbt/jaffle_shop_akshai --profiles-dir /appz/home/airflow/dags/dbt/jaffle_shop_akshai"
     )
 
     dbt_serve_docs_1 = BashOperator(
-        task_id="dbt_serve_docs",
+        task_id="dbt_serve_docs_1",
         bash_command = f"{dbt_executable_path} docs serve --target dev --project-dir /appz/home/airflow/dags/dbt/jaffle_shop_akshai --profiles-dir /appz/home/airflow/dags/dbt/jaffle_shop_akshai &"
     )
 
     dbt_generate_docs_2 = BashOperator(
-        task_id="dbt_generate_docs",
+        task_id="dbt_generate_docs_2",
         bash_command = f"{dbt_executable_path} docs generate --target dev --project-dir /appz/home/airflow/dags/dbt/data_engineering --profiles-dir /appz/home/airflow/dags/dbt/data_engineering"
     )
 
     dbt_serve_docs_2 = BashOperator(
-        task_id="dbt_serve_docs",
+        task_id="dbt_serve_docs_2",
         bash_command = f"{dbt_executable_path} docs serve --target dev --port 9090 --project-dir /appz/home/airflow/dags/dbt/data_engineering --profiles-dir /appz/home/airflow/dags/dbt/data_engineering &"
     )
 
