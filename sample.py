ec38bf1bc256765f180b59cf43a21d89a5d8e142
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-07-02 14:15:51 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,7 +4,7 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 
 def print_hello():
-    raise Exception('Intentional failure for PythonOperator!')
+    return 'Hello from PythonOperator!'
 
 default_args = {
     'owner': 'airflow',
@@ -18,16 +18,16 @@ dag = DAG(
     schedule_interval='@daily',
 )
 
-python_task = PythonOperator(
-    task_id='python_task',
-    python_callable=print_hello,
-    dag=dag,
-)
-
 bash_task = BashOperator(
     task_id='bash_task',
     bash_command='echo Hello from BashOperator!',
     dag=dag,
 )
 
-python_task >> bash_task
+python_task = PythonOperator(
+    task_id='python_task',
+    python_callable=print_hello,
+    dag=dag,
+)
+
+bash_task >> python_task
